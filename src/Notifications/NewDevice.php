3ed5fbdad5c645deb3f341e913984b832d82ab13
Bubka <858858+Bubka@users.noreply.github.com>
2024-04-20 17:50:37 +0200

    Remove useless functions

--- src/Notifications/NewDevice.php
+++ src/Notifications/NewDevice.php
@@ -5,8 +5,6 @@ namespace Bubka\LaravelAuthenticationLog\Notifications;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
-use Illuminate\Notifications\Messages\NexmoMessage;
-use Illuminate\Notifications\Messages\SlackMessage;
 use Illuminate\Notifications\Notification;
 use Bubka\LaravelAuthenticationLog\Models\AuthenticationLog;
 
@@ -49,32 +47,5 @@ class NewDevice extends Notification implements ShouldQueue
                 'browser' => $this->authenticationLog->user_agent,
                 'location' => $this->authenticationLog->location,
             ]);
-    }
-
-    public function toSlack($notifiable)
-    {
-        return (new SlackMessage())
-            ->from(config('app.name'))
-            ->warning()
-            ->content(__('Your :app account logged in from a new device.', ['app' => config('app.name')]))
-            ->attachment(function ($attachment) use ($notifiable) {
-                $attachment->fields([
-                    __('Account') => $notifiable->email,
-                    __('Time') => $this->authenticationLog->login_at->toCookieString(),
-                    __('IP Address') => $this->authenticationLog->ip_address,
-                    __('Browser') => $this->authenticationLog->user_agent,
-                    __('Location') =>
-                        $this->authenticationLog->location &&
-                        $this->authenticationLog->location['default'] === false ?
-                            ($this->authenticationLog->location['city'] ?? 'N/A') . ', ' . ($this->authenticationLog->location['state'] ?? 'N/A') :
-                            'Unknown',
-                ]);
-            });
-    }
-
-    public function toNexmo($notifiable)
-    {
-        return (new NexmoMessage())
-            ->content(__('Your :app account logged in from a new device.', ['app' => config('app.name')]));
     }
 }
